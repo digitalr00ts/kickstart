43f8c77821baee6212d6b9237047dbe2d9a3bdb4
Carlos Meza <hire@carlosmeza.com>
2016-04-17 03:54:19 -0700

    add missing vgname attribute

--- part.sh
+++ part.sh
@@ -18,8 +18,8 @@ echo "clearpart --initlabel --drives=$disk --all" >> $file
 echo "part /boot --fstype=ext4 --mkfsoptions=\"-Onone,extent,filetype,sparse_super,flex_bg,uninit_bg,resize_inode -I 128 -N 1000\" --fsoptions=\"rw,noatime,suid,dev,exec,auto,nouser,async,stripe=4\"--recommended --label=boot --asprimary --ondrive=$drive" >> $file
 echo "part pv.01 --grow --ondrive=$drive" >> $file 
 echo "volgroup vg_$disk pv.01" >> $file
-echo "logvol swap --hibernation --label=swap --name=lv_swap" >> $file
-echo "logvol none --thinpool --profile=thin-performance --grow --name=lv_$disk" >> $file
+echo "logvol swap --hibernation --vgname=vg_$disk --label=swap --name=lv_swap" >> $file
+echo "logvol none --vgname=vg_$disk --thinpool --profile=thin-performance --grow --name=lv_$disk" >> $file
 echo "logvol / --vgname=vg_$disk --size=8000 --thin --poolname=lv_$disk --fstype=ext4 --mkfsoptions=\"-Onone,extent,ext_attr,dir_index,filetype,sparse_super,flex_bg,uninit_bg,large_file,dir_nlink,resize_inode -I 128 -i 32768\" --fsoptions=\"rw,noatime,suid,dev,exec,auto,nouser,async\" --label=root --name=lv_root" >> $file
 echo "logvol /var --vgname=vg_$disk --size=2000 --thin --poolname=lv_$disk --fstype=ext4 --mkfsoptions=\"-Onone,extent,ext_attr,dir_index,filetype,sparse_super,flex_bg,uninit_bg,large_file,has_journal,dir_nlink,resize_inode -I 128 -i 32768\" --fsoptions=\"rw,relatime,lazytime,suid,dev,exec,auto,nouser,async\" --label=var --name=lv_var" >> $file
 echo "logvol /home --vgname=vg_$disk --size=8000 --thin --poolname=lv_$disk --fstype=ext4 --mkfsoptions=\"-Onone,extent,ext_attr,dir_index,filetype,sparse_super,flex_bg,uninit_bg,large_file,has_journal,dir_nlink,resize_inode -I 128 -i 32768\" --fsoptions=\"rw,relatime,lazytime,suid,dev,exec,auto,nouser,async\" --label=home --name=lv_home" >> $file
