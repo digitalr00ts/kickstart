0abf94ca52fd9da0d28833115899cb63c25317f8
Carlos Meza <carlos@digitalr00ts.com>
2016-04-20 15:05:35 -0700

    created dir and moved scripts

--- scripts/get-ks.sh
+++ scripts/get-ks.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 [ -z $1 ] && declare x='none' || x=$1
+startpath=$(pwd)
 runpath='/run/install'
 
 cd $runpath
@@ -16,13 +17,15 @@ if [ $x = '--all' ] ; then
   curl --location --remote-name https://github.com/digitalr00ts/korora-kickstart/raw/master/korora-xfce.ks
 fi
 
-curl --location --remote-name https://github.com/digitalr00ts/korora-kickstart/raw/master/disk.sh
-chmod +x ./disk.sh
-./disk.sh
-
 curl --location --remote-name https://github.com/digitalr00ts/korora-kickstart/raw/master/digitalr00ts-repo.ks
 curl --location --remote-name https://github.com/digitalr00ts/korora-kickstart/raw/master/korora-base.ks
 
-mkdir --parent $runpath/snippets && cd - && cd $runpath/snippets
+mkdir --parent ${runpath}/snippets && cd $_
 curl --location --remote-name https://github.com/digitalr00ts/korora-kickstart/raw/master/snippets/packagekit-cached-metadata.ks
-cd -
+
+mkdir --parent ${runpath}/scripts && cd $_
+curl --location --remote-name https://github.com/digitalr00ts/korora-kickstart/raw/master/scripts/disk.sh
+chmod +x ./disk.sh
+./disk.sh
+
+cd $startpath
