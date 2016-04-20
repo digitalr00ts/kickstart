30b9d7cb3d499e482cc8fe4607067246227b25d2
Carlos Meza <carlos@digitalr00ts.com>
2016-04-19 22:40:45 -0700

    add parameters

--- get-ks.sh
+++ get-ks.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 cd /run/install
+
 curl --location --remote-name https://github.com/digitalr00ts/korora-kickstart/raw/mini/part.sh
 chmod +x ./part.sh
 ./part.sh
+
 curl --location --remote-name https://github.com/digitalr00ts/korora-kickstart/raw/mini/digitalr00ts-repo.ks
 curl --location --remote-name https://github.com/digitalr00ts/korora-kickstart/raw/mini/korora-base.ks
+
 mkdir /run/install/snippets && cd - && cd /run/install/snippets
 curl --location --remote-name https://github.com/digitalr00ts/korora-kickstart/raw/mini/snippets/packagekit-cached-metadata.ks
-if $1 == '--all'; then
+
+if [ $1 != '--mini' ] ; then
+  curl --location --remote-name https://github.com/digitalr00ts/korora-kickstart/raw/mini/min.cfg
+elif [ $1 = '--all' ] ; then
   curl --location --remote-name https://github.com/digitalr00ts/korora-kickstart/raw/mini/digitalr00ts-korora-common-min.ks
   curl --location --remote-name https://github.com/digitalr00ts/korora-kickstart/raw/mini/digitalr00ts-xfce-packages.ks
   curl --location --remote-name https://github.com/digitalr00ts/korora-kickstart/raw/mini/fedora-live-minimization.ks
@@ -15,4 +21,5 @@ if $1 == '--all'; then
   curl --location --remote-name https://github.com/digitalr00ts/korora-kickstart/raw/mini/korora-common-packages.ks
   curl --location --remote-name https://github.com/digitalr00ts/korora-kickstart/raw/mini/korora-xfce.ks
 fi
+
 cd -
