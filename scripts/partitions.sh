8fddc7ec86b358a204a846b4389385235b8bd7d0
Carlos Meza <carlos@digitalr00ts.com>
2016-04-29 13:00:43 -0700

    corrected variables

--- scripts/partitions.sh
+++ scripts/partitions.sh
@@ -26,8 +26,8 @@ echo "bootloader --boot-drive=$disk --timeout=1" >> $file
 # --add-boot may cause Anaconda to crash when explicitly creating /boot
 echo "reqpart" >> $file
 
-echo "part /boot --recommended --fstype=ext4 --mkfsoptions=\"-O none,extent,extra_isize,ext_attr,filetype,sparse_super,flex_bg,uninit_bg,resize_inode -I 256 -N 1000\" --fsoptions=\"rw,noatime,suid,dev,exec,auto,nouser,async,stripe=4\" --label=boot --ondrive=$drive" >> $file
-echo "part pv.01 --grow --ondrive=$drive" >> $file
+echo "part /boot --recommended --fstype=ext4 --mkfsoptions=\"-O none,extent,extra_isize,ext_attr,filetype,sparse_super,flex_bg,uninit_bg,resize_inode -I 256 -N 1000\" --fsoptions=\"rw,noatime,suid,dev,exec,auto,nouser,async,stripe=4\" --label=boot --ondrive=$disk" >> $file
+echo "part pv.01 --grow --ondrive=$disk" >> $file
 echo "volgroup vg_$disk pv.01" >> $file
 echo "logvol swap --$([ ! $disk == 'vda' ] && echo 'hibernation' || echo 'recommended') --vgname=vg_$disk --name=lv_swap --fstype=swap --label=swap --size=512" >> $file
 echo "logvol none --vgname=vg_$disk --name=lv_$disk --thinpool --size=1000 --grow" >> $file
