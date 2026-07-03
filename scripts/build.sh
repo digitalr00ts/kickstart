213eae75cd25eb3c7c5529d01459dfde97cdbc1c
Carlos Meza <carlos@digitalr00ts.com>
2026-07-03 01:37:35 -0700

    fix(scripts): update to run in Lima VM

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,6 @@ set -euo pipefail
 
 VERSION=${1:-44}
 ARCH=${2:-$(uname -m)}
-LIMA_INSTANCE=kiwi-builder
-LIMA_TEMPLATE=lima/kiwi-builder.yaml
 KIWI_DESC=kiwi/fedora-${VERSION}.kiwi
 KIWI_DESC_DIR=${KIWI_DESC%/*}
 OUTPUT_DIR=${OUTPUT_DIR:-output}
@@ -12,26 +10,31 @@ PLATFORM="$(uname -s | tr '[:upper:]' '[:lower:]')"
 
 err() { echo "Error: $*" >&2; exit 1; }
 
-[[ $ARCH == arm64 ]] && ARCH=aarch64
-[[ $ARCH == amd64 ]] && ARCH=x86_64
-[[ -f $KIWI_DESC ]] || err "Missing: $KIWI_DESC"
-[[ -f $LIMA_TEMPLATE ]] || err "Missing: $LIMA_TEMPLATE"
+check-lima(){
+  [[ ! $PLATFORM == darwin ]] && return
+  local status
+  status="$(poe lima-cmd list -- --format '{{.Status}}')" || true
+  [[ -n "${status}" ]] || { poe lima-create; return; }
+  [[ "${status}" = "Stopped" ]] && poe lima-cmd start || true
+  OUTPUT_DIR=/opt/kiwi-build/output/
+}
 
-case "$ARCH" in
-  x86_64|aarch64) ;;
-  *) err "Unsupported architecture '$ARCH'. Use x86_64 or aarch64." ;;
-esac
+prefix() {
+  [[ ! $PLATFORM == darwin ]] && return
+  command -v limactl >/dev/null || err limactl not found.
+  echo "poe lima-cmd shell --"
+}
 
 build() {
-  mkdir -p "$OUTPUT_DIR"
+  local cmd_prefix
+  cmd_prefix=$($1)
 
-  local cmd_prefix=''
-  if [[ $PLATFORM == darwin ]]; then
-    local cmd_prefix="limactl shell $LIMA_INSTANCE"
-  fi
-
-  $cmd_prefix uv sync --group build --frozen
-  $cmd_prefix sudo uv run kiwi-ng \
+  # shellcheck disable=SC2068
+  ${cmd_prefix[@]} mkdir -p "$OUTPUT_DIR"
+  # shellcheck disable=SC2068
+  ${cmd_prefix[@]} uv sync --group build --frozen
+  # shellcheck disable=SC2068
+  ${cmd_prefix[@]} sudo uv run kiwi-ng \
     --type oem \
     --profile "$ARCH" \
     system build \
@@ -39,7 +42,14 @@ build() {
     --target-dir "$OUTPUT_DIR/$ARCH"
 }
 
-[[ $PLATFORM == darwin ]] && \
-(limactl list | grep "^${LIMA_INSTANCE}" | grep -q "Running" || limactl start --tty=false "$LIMA_TEMPLATE")
+[[ $ARCH == arm64 ]] && ARCH=aarch64
+[[ $ARCH == amd64 ]] && ARCH=x86_64
+[[ -f $KIWI_DESC ]] || err "Missing: $KIWI_DESC"
 
-build
+case "$ARCH" in
+  x86_64|aarch64) ;;
+  *) err "Unsupported architecture '$ARCH'. Use x86_64 or aarch64." ;;
+esac
+
+check-lima
+build prefix
